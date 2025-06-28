bd298cd1f74329a4953e5756fc2c54f4cb879c3d
sabkazz <90966310+KazikS@users.noreply.github.com>
2025-06-29 01:42:42 +0300

    specify img

--- src/components/ProjectCard/ProjectPreview.tsx
+++ src/components/ProjectCard/ProjectPreview.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { StaticImageData } from 'next/image'
 
-export default function ProjectPreview({image}: { image: any }) {
+export default function ProjectPreview({image}: { image: StaticImageData }) {
     return (
         <div className='relativ aspect-video'>
             <Image
